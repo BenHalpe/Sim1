dae7e226d72060ded6fa8d9daa8119fcbbefb23c
BenHalpe <benhalpe@gmail.com>
2020-05-27 13:57:32 +0300

    added fas and fas test, and updated test results in dry

--- fas.sv
+++ fas.sv
@@ -10,6 +10,26 @@ module fas (
 
 // Put your code here
 // ------------------
+logic g1Out, g3Out, g4Out, g5Out, g6Out, g7Out, g8Out, g9Out, g10Out, g11Out;
+
+XOR2 #(.Tpdlh(6),.Tpdhl(5)) g1 (.A(a), .B(b), .Z(g1Out));
+XOR2 #(.Tpdlh(6),.Tpdhl(5)) g2 (.A(g1Out), .B(cin), .Z(s));
+
+NOT #(.Tpdlh(10),.Tpdhl(9)) g3 (.A(b), .Z(g3Out));
+NOT #(.Tpdlh(10),.Tpdhl(9)) g4 (.A(cin), .Z(g4Out));
+
+OR2 #(.Tpdlh(10),.Tpdhl(8)) g5 (.A(g3Out), .B(g4Out), .Z(g5Out));
+NOT #(.Tpdlh(10),.Tpdhl(9)) g6 (.A(g5Out), .Z(g6Out));
+
+OR2 #(.Tpdlh(10),.Tpdhl(8)) g7 (.A(cin), .B(b), .Z(g7Out));
+NOT #(.Tpdlh(10),.Tpdhl(9)) g8 (.A(g7Out), .Z(g8Out));
+
+XOR2 #(.Tpdlh(6),.Tpdhl(5)) g9 (.A(a), .B(a_ns), .Z(g9Out));
+
+OR2 #(.Tpdlh(10),.Tpdhl(8)) g10 (.A(g8Out), .B(g9Out), .Z(g10Out));
+NOT #(.Tpdlh(10),.Tpdhl(9)) g11 (.A(g10Out), .Z(g11Out));
+
+OR2 #(.Tpdlh(10),.Tpdhl(8)) g12 (.A(g6Out), .B(g11Out), .Z(cout));
 
 
 // End of your code
--- fas_test.sv
+++ fas_test.sv
@@ -3,7 +3,35 @@ module fas_test;
 
 // Put your code here
 // ------------------
+logic tst_a;
+logic tst_b;
+logic tst_cin;
+logic tst_a_ns;
+logic tst_s;
+logic tst_cout;
 
+fas fasTest ( 
+	.a(tst_a),
+	.b(tst_b),
+	.cin(tst_cin),
+	.a_ns(tst_a_ns),
+	.s(tst_s),
+	.cout(tst_cout)
+);
+
+initial begin
+	tst_a = 0;
+	tst_b = 0;
+	tst_cin = 0;
+	tst_a_ns = 0;
+	#10
+	tst_a = 1;
+	#12 
+	tst_a = 0;
+	#10
+	tst_a = 0;
+
+end
 
 // End of your code
 
